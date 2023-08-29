2159f45ba251b2681ac845b6bfbd755fcade0835
LiekevdHeide <liekeheide@gmail.com>
2023-08-29 14:16:15 +0200

    Removed all print statements and irrelevant comments.

--- modelForm2.py
+++ modelForm2.py
@@ -67,12 +67,6 @@ def g_func(
     )
 
     if not (setting.threshold_pol_basic or setting.threshold_pol_cost):
-        # array of (not scheduling, scheduling) at time t + lead time
-        # cost_array = (
-        #     h_func(setting, remaining, schedule_no, phase, t),
-        #     [setting.shiftC[t + lead_time], 0, 0]
-        #     + h_func(setting, remaining, schedule_yes, phase, t),
-        # )
         cost_no = h_func(setting, remaining, schedule_no, phase, t)
         if setting.shiftC[t + lead_time] > setting.shiftC_avg:
             cost_yes = np.array([0, setting.shiftC[t + lead_time], 0, 0])
@@ -80,17 +74,9 @@ def g_func(
             cost_yes = np.array([setting.shiftC[t + lead_time], 0, 0, 0])
         cost_yes += h_func(setting, remaining, schedule_yes, phase, t)
         if sum(cost_yes) < sum(cost_no):
-            # print(f"{cost_yes=}")
             return cost_yes, 1
         else:
-            # print(f"{cost_no=}")
             return cost_no, 0
-        # cost_combi = (sum(cost_array[0], sum(cost_array[1])))
-        # print(cost_array, cost_combi)
-        # choice = np.where(cost_combi == np.min(cost_combi))
-        # print(choice)
-        # return min(cost_array), cost_array.index(min(cost_array))
-        return cost_array[choice], choice
 
     # Calculate threshold measure
     threshold_schedule_decision = calc_threshold.measure(
@@ -116,14 +102,12 @@ def g_func(
 
 @functools.cache
 def l_1(setting, phase: int) -> float:
-    # print("l_1")
     return np.array([0.0, 0.0, sum(setting.phaseC[phase: setting.NumPhases]), 0.0])
 
 
 @functools.cache
 def l_2(setting, time: int) -> float:
     # Finished previous time step -> time - 1
-    # print("l_2")
     return np.array([0.0, 0.0, 0.0, (setting.Deadline - (time - 1)) * setting.earlyC])
 
 
@@ -154,16 +138,6 @@ def k_func(
         rem_non_neg = max(remaining - setting.E_values[epsilon], 0)
         if rem_non_neg == 0:
             if n < setting.NumPhases - 1:
-                # cost += setting.E_probs[epsilon] * (
-                #     f_func(
-                #         setting,
-                #         setting.WorkPerPhase[n + 1],
-                #         schedule,
-                #         n + 1,
-                #         t + 1,
-                #     )
-                # )
-                # print(f"f1:{cost=}")
                 cost += [setting.E_probs[epsilon] * x for x in f_func(
                         setting,
                         setting.WorkPerPhase[n + 1],
@@ -172,17 +146,8 @@ def k_func(
                         t + 1,
                     )]
             else:
-                # cost += setting.E_probs[epsilon] * (
-                #     f_func(setting, 0, schedule, n, t + 1)
-                # )
-                # print(f"f2:{cost=}")
                 cost += [setting.E_probs[epsilon] * x for x in f_func(setting, 0, schedule, n, t + 1)]
         # calculate future cost if this epsilon indeed occurs
         else:
-            # print("f=", f_func(setting, rem_non_neg, schedule, n, t + 1))
-            # cost += setting.E_probs[epsilon] * (
-            #     f_func(setting, rem_non_neg, schedule, n, t + 1)
-            # )
             cost += [setting.E_probs[epsilon] * x for x in f_func(setting, rem_non_neg, schedule, n, t + 1)]
-            # print(f"f_cost= {cost}")
     return cost
