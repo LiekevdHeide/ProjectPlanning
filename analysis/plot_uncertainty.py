ed461568e5b224e8eb9478307c35731282dc4f78
LiekeThuis <liekeheide@gmail.com>
2024-01-02 17:21:42 +0100

    Only optimal & combine in 1 file

--- analysis/plot_uncertainty.py
+++ analysis/plot_uncertainty.py
@@ -10,12 +10,19 @@ rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
 rc('text', usetex=True)
 rc('font', **{'size': 18})
 
+
 def parse_inputs():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-f",
-        "--file",
+        "-fu",
+        "--uncertaintyFile",
         help="Input file, .csv with all uncertainty output",
+        required=True,
+    )
+    parser.add_argument(
+        "-fo",
+        "--overtimeFile",
+        help="Input file, .csv with all overtime output",
         required=True,
     )
     parser.add_argument(
@@ -30,58 +37,55 @@ def parse_inputs():
 # get input file containing all experiments
 parser = parse_inputs()
 
-# -------------check completeness of experiments-------------------------------
-fileToRead = parser.file
-output_og = pd.read_csv(fileToRead, header="infer", sep=";")
-output = clean_output_files.clean(output_og)
-# -----------------------------------------------------------------------------
-# Group columns:
-cost_columns = [
-    "solution_cost_o", "solution_cost_b", "solution_cost_c"
-]
-cost_column_labels = [
-    "Optimal", "Basic", "Shift-based"
-]
+filesToRead = [parser.uncertaintyFile, parser.overtimeFile]
+columnsToRead = ["Uncertainty", "overtimeC"]
+labelsToRead = ["Uncertainty", "Overtime cost"]
 
-# Split the uncertainties column
-output = pd.concat([output, output["E_probs"].str.split(', ', expand=True)], axis=1)
-output.rename({1: "Uncertainty"}, axis=1, inplace=True)
-output["Uncertainty"] = 1 - pd.to_numeric(output["Uncertainty"])
+for files in range(2):
+    # -------------check completeness of experiments-------------------------------
+    fileToRead = filesToRead[files]
+    output_og = pd.read_csv(fileToRead, header="infer", sep=";")
+    output = clean_output_files.clean(output_og)
+    # -----------------------------------------------------------------------------
+    # Group columns:
+    column_labels = [
+        "Lead time 0", "Lead time 2", "Lead time 14"
+    ]
+    if files == 0:
+        # Split the uncertainties column
+        output = pd.concat([output, output["E_probs"].str.split(', ', expand=True)], axis=1)
+        output.rename({1: "Uncertainty"}, axis=1, inplace=True)
+        output["Uncertainty"] = 1 - pd.to_numeric(output["Uncertainty"])
 
-# Make plots of impact of uncertainty:
-leadTimes = output["LeadTime"].unique()
-for idx_l in range(0, leadTimes.size, 1):
-    plotUncertainty = output[(output["LeadTime"] == leadTimes[idx_l])]
-    plotUncertainty = plotUncertainty.sort_values(by=["Uncertainty"], ascending=False)
-    print(plotUncertainty['Uncertainty'].unique())
+    # Make plots of impact of uncertainty:
+    leadTimes = output["LeadTime"].unique()
 
     fig, ax = plt.subplots()
     markerStyles = ["o", "s", "^"]
-    for idx_c in range(0, 3, 1):
-        ax.scatter(x=plotUncertainty["Uncertainty"], y=plotUncertainty[cost_columns[idx_c]],
-                   label=f"{cost_column_labels[idx_c]}",
-                   edgecolor="black", facecolor='none', marker=markerStyles[idx_c])
+    for idx_l in range(leadTimes.size):
+        plot = output[(output["LeadTime"] == leadTimes[idx_l])]
+        plot = plot.sort_values(by=columnsToRead[files], ascending=False)
+        ax.scatter(x=plot[columnsToRead[files]], y=plot["solution_cost_o"],
+                   label=f"{column_labels[idx_l]}",
+                   edgecolor="black", facecolor='none', marker=markerStyles[idx_l])
 
     # set locations and names of the labels
-    plt.xticks([0.0, 0.1, 0.2, 0.3, 0.4])
-    plt.ylim(bottom=28, top=52)
-    plt.xlabel("Uncertainty")
+    if files == 0:
+        plt.xticks([0.0, 0.1, 0.2, 0.3, 0.4])
+    if files == 1:
+        plt.xticks([1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6])
+
+    plt.ylim(bottom=28, top=40)
+    plt.xlabel(labelsToRead[files])
     plt.ylabel("Cost")
     plt.legend(frameon=False, loc="upper left")
     fig.tight_layout()
 
     if parser.savePlot:
         plt.savefig(
-              f"Impact uncertainty lead time {leadTimes[idx_l]}.pdf"
+              f"Impact {columnsToRead[files]} lead time {leadTimes[idx_l]}.pdf"
         )
     else:
-        plt.title(f"Impact of uncertainty for lead time {leadTimes[idx_l]}")
+        plt.title(f"Impact of {columnsToRead[files]} for lead time {leadTimes[idx_l]}")
 
     plt.show()
-
-    # fig, ax = plt.subplots()
-    # plotUncertainty.plot(ax=ax, x="Uncertainty", y=cost_columns, kind="line")
-    # plt.xticks([0.0, 0.1, 0.2, 0.3, 0.4])
-    # plt.title(f"Lead time {leadTimes[idx_l]}")
-    # fig.tight_layout()
-    # plt.show()
